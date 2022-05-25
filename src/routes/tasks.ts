1bffa52b11a3a00480f7d66ff5d24a4da324386e
Dominic Lowes <chaiskeng@gmail.com>
2022-05-25 17:26:25 +0900

    Feat: set task routes

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,6 +1,13 @@
 import express from "express";
 
+const taskController = require("../controllers/taskcontroller");
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", taskController.getAllTasks);
+router.get("/:id", taskController.getTasksById);
+router.post("/", taskController.saveTask);
+router.put("/:id", taskController.saveTask);
+router.delete("/:id", taskController.deleteTask);
+
+export default router;
