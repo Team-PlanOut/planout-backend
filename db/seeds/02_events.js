00e6008d7a2dfef7227959b58f4e71ff110b1560
Konstantin Shambazov <k.shambazov@gmail.com>
2022-05-26 15:26:02 +0900

    bug: fix seeds

--- db/seeds/02_events.js
+++ db/seeds/02_events.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('events').del()
   await knex('events').insert([
-    {host: 1, event_name: "Ruprecht's Birthday", date: "2022-06-23T18:25:43.511Z", budget: 1000000 },
-    {host: 2, event_name: "Ruprecht's Funeral", date: "2022-06-24T18:25:43.511Z", budget: 15 },
-    {host: 3, event_name: "Day of Judgement", date: "2026-06-06T18:25:43.511Z", budget: 666 },
+    {id:1, host: 1, event_name: "Ruprecht's Birthday", date: "2022-06-23T18:25:43.511Z", budget: 1000000 },
+    {id:2, host: 2, event_name: "Ruprecht's Funeral", date: "2022-06-24T18:25:43.511Z", budget: 15 },
+    {id:3, host: 3, event_name: "Day of Judgement", date: "2026-06-06T18:25:43.511Z", budget: 666 },
   ]);
 };
