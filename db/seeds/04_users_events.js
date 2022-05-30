c5dfb9a0d9217dd78eb73b5fa15461ae97e45089
yccjs <yccjsalvador@gmail.com>
2022-05-30 21:47:25 +0900

    chore:edit middleware and add logic, fix cors issue, save user data to db

--- db/seeds/04_users_events.js
+++ db/seeds/04_users_events.js
@@ -4,16 +4,16 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('users_events').del();
-  await knex('users_events').insert([
-    { id: 1, event_id: 1, user_id: 1 },
-    { id: 2, event_id: 1, user_id: 2 },
-    { id: 3, event_id: 1, user_id: 3 },
-    { id: 4, event_id: 2, user_id: 1 },
-    { id: 5, event_id: 2, user_id: 2 },
-    { id: 6, event_id: 2, user_id: 3 },
-    { id: 7, event_id: 3, user_id: 1 },
-    { id: 8, event_id: 3, user_id: 2 },
-    { id: 9, event_id: 3, user_id: 3 },
+  await knex("users_events").del();
+  await knex("users_events").insert([
+    { id: 1, event_id: 1, user_id: 100 },
+    { id: 2, event_id: 1, user_id: 200 },
+    { id: 3, event_id: 1, user_id: 300 },
+    { id: 4, event_id: 2, user_id: 100 },
+    { id: 5, event_id: 2, user_id: 200 },
+    { id: 6, event_id: 2, user_id: 300 },
+    { id: 7, event_id: 3, user_id: 100 },
+    { id: 8, event_id: 3, user_id: 200 },
+    { id: 9, event_id: 3, user_id: 300 },
   ]);
 };
