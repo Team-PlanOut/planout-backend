fa4c468dd57f8e498bdb2bcf31c2cd168e773333
Dominic Lowes <chaiskeng@gmail.com>
2022-06-05 18:24:02 +0900

    Update: refactor friends controller and rename friends seed file
    Co-authored-by: juliwithoutthee <juliwithoutthee@users.noreply.github.com>

--- src/controllers/friendscontroller.ts
+++ src/controllers/friendscontroller.ts
@@ -1,26 +1,25 @@
 import { Request, Response } from "express";
-const friendsModel = require("../models/friends")
+const friendsModel = require("../models/friends");
 
 module.exports = {
-    async getAllFriends  (req: Request, res: Response) {
-    const allFriends = await friendsModel.getAllFriends()
+  async getAllFriends(req: Request, res: Response) {
+    const allFriends = await friendsModel.getAllFriends();
     res.send(allFriends);
-},
-async getFriendsByUserId  (req: Request, res: Response){
+  },
+  async getFriendsByUserId(req: Request, res: Response) {
     const id = req.params.id;
     const myFriends = await friendsModel.getFriendsByUserId(id);
     res.send(myFriends);
-},
-async saveFriendship(req: Request, res: Response) {
-    const {user1_id, user2_id} =req.body;
+  },
+  async saveFriendship(req: Request, res: Response) {
+    const { user1_id, user2_id } = req.body;
+    await friendsModel.addFriend(req.body);
+    res.status(200);
+  },
 
-},
-
-async deleteFriendship  (req: Request, res: Response) {
+  async deleteFriendship(req: Request, res: Response) {
     const id = req.params.id;
     await friendsModel.deleteTask(id);
-}
-}
-
-
-
+    res.status(200).send("Destroyed Friendship");
+  },
+};
