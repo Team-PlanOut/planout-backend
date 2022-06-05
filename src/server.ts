5ba943551e3023ab7e62f593796eedcd623d8f8e
Dominic Lowes <chaiskeng@gmail.com>
2022-06-05 16:15:09 +0900

    Feat: add stripe routes to server

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || "8080";
-const Stripe = require("stripe");
-const stripe = Stripe(/*STRIPE API KEY GOES HERE */)
+// 
 
 const middleware = require("./middleware/index");
 
@@ -13,6 +12,7 @@ const eventRoutes = require("./routes/events");
 const userRoutes = require("./routes/users");
 const taskRoutes = require("./routes/tasks");
 const eventUserRoutes = require("./routes/event_users");
+const stripeRoutes = require("./routes/stripe");
 
 function solveCorsIssue(req: Request, res: Response, next: any) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -32,6 +32,7 @@ app.use("/tasks", taskRoutes);
 app.use("/events", eventRoutes);
 app.use("/users", userRoutes);
 app.use("/eventusers", eventUserRoutes);
+app.use("/stripe", stripeRoutes);
 
 
 app.listen(PORT, () => {
