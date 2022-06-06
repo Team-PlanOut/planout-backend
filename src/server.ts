dc1da464ddbe4030db772930a600cc739fd27bae
Konstantin Shambazov <k.shambazov@gmail.com>
2022-06-06 13:50:16 +0900

    bug: remove stripe integration

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,11 @@ const app = express();
 const PORT = process.env.PORT || "8080";
 // 
 
-const middleware = require("./middleware/index");
 
 const eventRoutes = require("./routes/events");
 const userRoutes = require("./routes/users");
 const taskRoutes = require("./routes/tasks");
 const eventUserRoutes = require("./routes/event_users");
-const stripeRoutes = require("./routes/stripe");
 const friendRoutes = require("./routes/friends");
 
 function solveCorsIssue(req: Request, res: Response, next: any) {
@@ -28,12 +26,11 @@ function solveCorsIssue(req: Request, res: Response, next: any) {
 app.use(solveCorsIssue);
 app.use(express.json());
 app.use(cors());
-app.use(middleware.decodeToken);
+// app.use(middleware.decodeToken);
 app.use("/tasks", taskRoutes);
 app.use("/events", eventRoutes);
 app.use("/users", userRoutes);
 app.use("/eventusers", eventUserRoutes);
-app.use("/stripe", stripeRoutes);
 app.use("/friends", friendRoutes);
 
 
